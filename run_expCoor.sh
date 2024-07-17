810bf3e2b6c69b7b4691719ebccb881ab6c5dc4e
zsxqblz <zsxqblz@gmail.com>
2024-07-16 22:51:18 -0400

    240716d

--- run_expCoor.sh
+++ run_expCoor.sh
@@ -4,21 +4,23 @@
 #SBATCH --ntasks=1               # total number of tasks across all nodes
 #SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G is default)
-#SBATCH --time=4:00:00          # total run time limit (HH:MM:SS)
+#SBATCH --time=23:00:00          # total run time limit (HH:MM:SS)
 #SBATCH --mail-type=end          # send email when job ends
+#SBATCH --array=0-9
 #SBATCH --mail-user=yz4281@princeton.edu
 
-let i=$1
+let "i=$SLURM_ARRAY_TASK_ID+1"
+# let "i=$1+1"
 let dx=100
 let dy=100
-let dt=100
+let dt=300
 let pdf=0.25
-let pdr=0.25
+let "pdr=0.01*i"
 let panh=1
-let pgen=0.01
-let nsim=10000
+let "pgen=0.001"
+let nsim=30000
 
-let id=0+i
-let date=231025
+let "id=40+i"
+let date=240630
 
 julia run_expCoor.jl $dx $dy $dt $pdf $pdr $panh $pgen $nsim data/${date}/${date}_d${id}_
